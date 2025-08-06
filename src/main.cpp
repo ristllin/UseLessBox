0447357ac142089add3b1200db8461ffa3932fd5
Roy Darnell <32160010+ristllin@users.noreply.github.com>
2025-08-06 07:06:51 +0100

    restructure

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,38 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+Servo myservo;
+const int rocker_pos = 150;
+const int switch_pos = 25;
+const int min_wait = 0;    //in miliseconds
+const int max_wait = 3000; //in miliseconds
+int rand_wait = 0;    // large num is slow
+int servo_speed = 15;    // large num is slow
+int servo_min_speed = 50;    // large num is slow
+int servo_max_speed = 0;    // large num is slow
+
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  myservo.attach(3); 
+  myservo.write(35);  
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+  servo_speed = random(servo_max_speed, servo_min_speed);
+  Serial.println(servo_speed);
+  rand_wait = random(min_wait, max_wait);
+  Serial.println(rand_wait);
+  delay(rand_wait);
+  for (int pos = switch_pos; pos <= rocker_pos; pos += 1) {
+    myservo.write(pos);             
+    delay(servo_speed);                       
+  }
+  Serial.println(servo_speed);
+  rand_wait = random(min_wait, max_wait);
+  Serial.println(rand_wait);
+  for (int pos = rocker_pos; pos > switch_pos; pos -= 1) {
+    myservo.write(pos);             
+    delay(servo_speed);                       
+  }
 }
